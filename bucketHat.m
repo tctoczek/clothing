14d971420f7f65bc32b9f481ea17d48d6dc5f267
tctoczek <64232955+tctoczek@users.noreply.github.com>
2020-04-25 17:52:11 -0400

    Update bucketHat.m

--- bucketHat.m
+++ bucketHat.m
@@ -2,7 +2,7 @@ function [replaceMeBasedOnVariableListBelow] = bucketHat(replaceMeBasedOnVariabl
 %circleSkirt Summary of this function goes here
 %   List of variables
 %   Inputs: headCir
-%   Outputs: circleRad, hatHeight, flapLength
+%   Outputs: circleRad, hatHeight, brimWidth
 % **i'm not sure abt the variables cuz idk the pattern for a bucket hat
 % HAHA
 
