8b56584cc1ddbc48ac68551bed0ad71b4e8f99ba
sat747 <52468742+sat747@users.noreply.github.com>
2020-04-25 17:35:47 -0400

    Updated working slipPants function

--- slipPants.m
+++ slipPants.m
@@ -1,7 +1,30 @@
-function [replaceMeBasedOnVariableListBelow] = slipPants(replaceMeBasedOnVariableListBelow)
+function [topCut, bottomCut, innerCut, outerCut, garterLength, waistbLength, waistbWidth] = slipPants(waistCir, hipCir, calfCir, crotchHeight, pantLength, garterWidth)
 %circleSkirt Summary of this function goes here
 %   List of variables
 %   Inputs: waistCir, hipCir, thighCir, calfCir, crotchHeight, pantLength, garterWidth
 %   Outputs: topCut, bottomCut, innerCut, outerCut, garterLength, waistbLength, waistbWidth
+
+seamAllowance = 3/8;
+
+%topcut = 1/4 of hipCir + 2*seamAllowance + 1/4" fit allowance
+topCut = (hipCir/4) + 2*seamAllowance + (1/4); 
+
+%bottomCut = calfCir/2 + 2" fit allowance + 2*seamAllowance
+bottomCut = (calfCir/2) + 2 + 2*seamAllowance;
+
+%innerCut = pantLength - crotchHeight + 2*seamAllowance
+innerCut = pantLength - crotchHeight + 2*seamAllowance;
+
+%outerCut = pantLength + seam allowances
+outerCut = pantLength + 2*seamAllowance;
+
+%garterLength = waistCir - 1" + seam allowance
+garterLength = waistCir - 1 + seamAllowance;
+
+%waistbandlength = 4* topcut - 6*seamAllowance
+waistbLength = (4*topCut) - 6*seamAllowance;
+
+%waistband width = garterwidth*2 + 1/4" allowance + 2*seamAllowance
+waistbWidth = 2*garterWidth + (1/4) + 2*seamAllowance;
 
 end
