2574a3c0b82a91056d1b00dbfe876aea6c93bf64
sat747 <52468742+sat747@users.noreply.github.com>
2020-04-25 19:00:59 -0400

    Updated working halterTop function

--- halterTop.m
+++ halterTop.m
@@ -1,8 +1,25 @@
-function [replaceMeBasedOnVariableListBelow] = halterTop(replaceMeBasedOnVariableListBelow)
+function [bodLength, bodHeight, bodHalterHeight, bodHalterWidth, strapLength] = halterTop(waistCir, topLength, halterWidth, halterHeight)
 %circleSkirt Summary of this function goes here
 %   List of variables
-%   Inputs: chestCir, bustCir, waistCir, topLength, neckWidth, halterHeight
+%   Inputs: waistCir, topLength, halterWidth, halterHeight
 %   Outputs: bodLength, bodHeight, bodHalterHeight, bodHalterWidth,
 %   strapLength
+
+seamAllowance = 3/8;
+
+%bodLength = waistCir + 2*seamAllowance
+bodLength = waistCir + 2*seamAllowance;
+
+%bodHeight = desired top length + allowances
+bodHeight = topLength + 2*seamAllowance; 
+
+%bodHalterHeight = desired halterheight + desired top length + allowances
+bodHalterHeight = halterHeight + topLength + 2*seamAllowance;
+
+%bodHalterWidth = desired halter width + seam allowance
+bodHalterWidth = halterWidth + 2*seamAllowance; 
+
+%strapLength = arbitrary, = waist cir 
+strapLength = waistCir;
 
 end
