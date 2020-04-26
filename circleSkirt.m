fbcaa634b2bf0412ad2a40d861adcdb252bf9e3b
tctoczek <64232955+tctoczek@users.noreply.github.com>
2020-04-26 16:05:48 -0400

    Update circleSkirt.m

--- circleSkirt.m
+++ circleSkirt.m
@@ -4,25 +4,23 @@ function [bigDiam, smallDiam, zipLength, waistbLength, waistbWidth] = circleSkir
 %   Inputs: waistCir, hipCir, skirtLength, waistbHeight
 %   Outputs: bigDiam, smallDiam, zipLength, waistbLength, waistbWidth
 
-seamAllowance = 1/2
-hem = 1
+seamAllowance = 1/2;
+hem = 1;
 
 %bigDiam = 2*skirtLength + smallDiam + 2*seamAllowance + 2*hem
-bigDiam = (2*skirtLength) + smallDiam + 2*seamAllowance + 2*hem
+bigDiam = (2*skirtLength) + smallDiam + 2*seamAllowance + 2*hem;
 
 %smallDiam = (waistCir+2*seamAllowance/pi)
-smallDiam = ((waistCir + (2*seamAllowance)) /pi)
+smallDiam = ((waistCir + (2*seamAllowance)) /pi);
 
 %zipLength = (1/2)*(hipCir-waistCir)
-zipLength = (1/2)*(hipCir-waistCir)
+zipLength = (1/2)*(hipCir-waistCir);
 
 %waistbLength = waistCir + 2*seamAllowance
-wasitbLength = waistCir + (2*seamAllowance)
+wasitbLength = waistCir + (2*seamAllowance);
 
 %waistbWidth = (waistbHeight) + seamAllowance + hem
-waistbWidth = (waistbHeight) + seamAllowance + hem
-
-
+waistbWidth = (waistbHeight) + seamAllowance + hem;
 
 end
 
