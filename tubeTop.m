bdb671da8b22fd37b739c87578d9e0602df1bd3e
tctoczek <64232955+tctoczek@users.noreply.github.com>
2020-04-26 15:50:23 -0400

    Update tubeTop.m

--- tubeTop.m
+++ tubeTop.m
@@ -1,7 +1,22 @@
-function [replaceMeBasedOnVariableListBelow] = tubeTop(replaceMeBasedOnVariableListBelow)
+function [chestCir, bustCir, waistCir, topLength] = tubeTop(bodLength, bodWidth, garterLength, garterWidth)
 %circleSkirt Summary of this function goes here
 %   List of variables
-%   Inputs: chestCir, bustCir, waistCir, topLength, garterWidth
-%   Outputs: bodLength, bodWidth, garterLength
+%   Inputs: chestCir, bustCir, waistCir, topLength
+%   Outputs: bodLength, bodWidth, garterLength, garterWidth
+
+seamAllowance = 3/8
+hem = 1/2
+
+% bodLength = topLength + 2*hem
+bodLength = topLength = (2*hem)
+
+% bodWidth = bustCir + 2*seamAllowance
+bodWidth = bustCir + (2*seamAllowance)
+
+% garterLength = chestCir + 2*seamAllowance
+garterLength = chestCir + (2*seamAllowance)
+
+% garterWidth = (bodLength/10)
+garterWidth = (bodLength/10)
 
 end
