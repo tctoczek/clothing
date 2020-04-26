f4d720ded11134f98127832af47b8bced6504fce
tctoczek <64232955+tctoczek@users.noreply.github.com>
2020-04-26 16:07:37 -0400

    Update tubeTop.m

--- tubeTop.m
+++ tubeTop.m
@@ -4,19 +4,19 @@ function [chestCir, bustCir, waistCir, topLength] = tubeTop(bodLength, bodWidth,
 %   Inputs: chestCir, bustCir, waistCir, topLength
 %   Outputs: bodLength, bodWidth, garterLength, garterWidth
 
-seamAllowance = 3/8
-hem = 1/2
+seamAllowance = 3/8;
+hem = 1/2;
 
 % bodLength = topLength + 2*hem
-bodLength = topLength = (2*hem)
+bodLength = topLength = (2*hem);
 
 % bodWidth = bustCir + 2*seamAllowance
-bodWidth = bustCir + (2*seamAllowance)
+bodWidth = bustCir + (2*seamAllowance);
 
 % garterLength = chestCir + 2*seamAllowance
-garterLength = chestCir + (2*seamAllowance)
+garterLength = chestCir + (2*seamAllowance);
 
 % garterWidth = (bodLength/10)
-garterWidth = (bodLength/10)
+garterWidth = (bodLength/10);
 
 end
