94cfb985b565f3c0c030a10ce6b07367747f63f6
tctoczek <64232955+tctoczek@users.noreply.github.com>
2020-04-26 16:06:50 -0400

    Update pencilSkirt.m

--- pencilSkirt.m
+++ pencilSkirt.m
@@ -4,23 +4,23 @@ function [waistCir, hipCir, skirtDepth, waistbHeight] = pencilSkirt(skirtLength,
 %   Inputs: waistCir, hipCir, skirtDepth, waistbHeight
 %   Outputs: skirtLength, skirtHeight, zipLength, waistbLength, waistbWidth
 
-seamAllowance = 3/8
-hem = 1
+seamAllowance = 3/8;
+hem = 1;
 
 % skirtLength = (waistCir/2) + 2* seamAllowance
-skirtLength = (waistCir/2) + (2*seamAllowance)
+skirtLength = (waistCir/2) + (2*seamAllowance);
 
 % skirtHeight = skirtDepth + seamAllowance + hem
-skirtHeight = skirtDepth + seamAllowance + hem
+skirtHeight = skirtDepth + seamAllowance + hem;
 
 % zipLength =(hipCir - waistCir)/2 + waistbHeight
-zipLength = ((1/2) * ( hipCir - waistCir )) + waistbHeight
+zipLength = ((1/2) * ( hipCir - waistCir )) + waistbHeight;
 
 % waistbLength = waistCir + 2*seamAllowance
-waistbLength = waistCir + (2*seamAllowance)
+waistbLength = waistCir + (2*seamAllowance);
 
 %waistbWidth = waistbheight + hem + seamAllowance
-waistbWidth = waistbheight + hem + seamAllowance
+waistbWidth = waistbheight + hem + seamAllowance;
 
 
 end
