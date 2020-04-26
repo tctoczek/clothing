a3f6d031f7130a073e9be512523ebd04558a77a3
tctoczek <64232955+tctoczek@users.noreply.github.com>
2020-04-26 14:42:13 -0400

    Update circleSkirt.m

--- circleSkirt.m
+++ circleSkirt.m
@@ -1,8 +1,28 @@
 function [bigDiam, smallDiam, zipLength, waistbLength, waistbWidth] = circleSkirt(waistCir,hipCir, skirtLength, waistbHeight, zipper)
 %circleSkirt Summary of this function goes here
 %   List of variables
-%   Inputs: waistCir,hipCir, skirtLength, waistbHeight, zipper
+%   Inputs: waistCir, hipCir, skirtLength, waistbHeight
 %   Outputs: bigDiam, smallDiam, zipLength, waistbLength, waistbWidth
+
+seamAllowance = 1/2
+hem = 1
+
+%bigDiam = 2*skirtLength + smallDiam + 2*seamAllowance + 2*hem
+bigDiam = (2*skirtLength) + smallDiam + 2*seamAllowance + 2*hem
+
+%smallDiam = (waistCir+2*seamAllowance/pi)
+smallDiam = ((waistCir + (2*seamAllowance)) /pi)
+
+%zipLength = (1/2)*(hipCir-waistCir)
+zipLength = (1/2)*(hipCir-waistCir)
+
+%waistbLength = waistCir + 2*seamAllowance
+wasitbLength = waistCir + (2*seamAllowance)
+
+%waistbWidth = (waistbHeight) + seamAllowance + hem
+waistbWidth = (waistbHeight) + seamAllowance + hem
+
+
 
 end
 
